86dcf596725c31f0468b72cbe805897ad0bbd007
wuyah <72677225+wuyah@users.noreply.github.com>
2020-10-13 18:19:55 +0800

    Add files via upload

---
+++ snake.h
@@ -0,0 +1,46 @@
+#pragma once
+#include<graphics.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+#include<iostream>
+
+
+enum Direction { Dir_up = 1, Dir_right = 2, Dir_down = 3, Dir_left = 4 };//fangxiangcanshu
+
+
+
+/////////////////////////定义蛇的节点和类
+
+typedef struct Node //蛇的节点
+{
+	int x;
+	int y;
+	struct Node *next;
+	struct Node *pre;
+} SnakeBody;
+
+class Snake //蛇的类
+{
+private:
+	SnakeBody *head;
+	SnakeBody *tail;
+	int direction;
+	int len;	   //节点数目
+
+
+public:
+	Snake();
+	void Move(Snake&snake);
+	//int getlen();
+	void eatfood(int x,int y );
+	void Chdir();//改变方向
+	void drawsnakebody(Node node);
+	void drawsnakehead(Node node);
+	//bool collidesnake(const Snake snake);   //碰撞自身检测
+	//bool collidefood(const Snake snake);
+	void drawall(Snake snake);
+	void coversnake(Node &node);
+	void clearsnakenode(Node node);
+	void eatfood();
+};
